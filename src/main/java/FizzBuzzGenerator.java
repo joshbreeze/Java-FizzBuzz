b35d00e703e65f8c92cb408ecde829b30665e043
Josh Breeze <josh@joshbreeze.co.uk>
2020-04-28 18:31:23 +0100

    Add test

--- src/main/java/FizzBuzzGenerator.java
+++ src/main/java/FizzBuzzGenerator.java
@@ -1,2 +1,10 @@
 public class FizzBuzzGenerator {
-}
+
+    public String getFizzBuzz(int num) {
+            String output = "";
+            if ((num % 3 == 0) && (num % 5 == 0)) output = "FizzBuzz";
+            if (num % 3 == 0) output = "Fizz";
+            if (num % 5 == 0) output = "Buzz";
+            return output;
+        }
+    }
--- src/test/java/FizzBuzzGeneratorTest.java
+++ src/test/java/FizzBuzzGeneratorTest.java
@@ -1,9 +1,21 @@
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
+import static org.assertj.core.api.Assertions.assertThat;
 public class FizzBuzzGeneratorTest {
+    private FizzBuzzGenerator fizzBuzzGenerator;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzzGenerator = new FizzBuzzGenerator();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"3,Fizz", "6,Fizz"})
+    void multiplesOfThreeReturnsFizz(int input, String result){
+        String fizzBuzz = fizzBuzzGenerator.getFizzBuzz(input);
+        assertThat(fizzBuzz).isEqualTo(result);
+    }
 
 //    Things to add:
 //    @BeforeEach
